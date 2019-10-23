697cbfc6b047b97245c6653964b36750aac2c675
geez0219 <geez0219@gmail.com>
2019-10-23 21:45:29 +0000

    new setup

--- setup.py
+++ setup.py
@@ -1,38 +1,20 @@
 import os
 import re
-import sys
-import argparse
-import datetime
+
 from setuptools import find_packages, setup
 
-# Create a environment variable to specify whether it is nightly build
-environment_variable_name = 'FASTESTIMATOR_IS_NIGHTLY'
-environment_variable_value = os.environ.get(environment_variable_name, None)
 
-if environment_variable_value is not None:
-    sys.stderr.write("Using '{}={}' environment variable!\n".format( \
-            environment_variable_name, environment_variable_value))
-
-def get_version(is_nightly):
+def get_version():
     path = os.path.dirname(__file__)
     version_re = re.compile(r'''__version__ = ['"](.+)['"]''')
     with open(os.path.join(path, 'fastestimator', '__init__.py')) as f:
         init = f.read()
-    if is_nightly:
-        now = datetime.datetime.now()
-        return "{}-{}{}{}".format(version_re.search(init).group(1), now.year, now.month, now.day)
-    else:
-        return version_re.search(init).group(1)
+    return version_re.search(init).group(1)
 
-def get_name(is_nightly):
-    if is_nightly:
-        return "fastestimator-nightly"
-    else:
-        return "fastestimator"
 
 setup(
-    name=get_name(environment_variable_value) ,
-    version=get_version(environment_variable_value),
+    name="fastestimator",
+    version=get_version(),
     description="Deep learning Application framework",
     packages=find_packages(),
     package_dir={'': '.'},
@@ -64,7 +46,9 @@ setup(
         'tqdm',
         'opencv-python',
         'papermill',
-        'tf-explain'
+        'tf-explain',
+        'slackclient',
+        'nest_asyncio'
     ],
     # Declare extra set for installation
     extras_require={},
